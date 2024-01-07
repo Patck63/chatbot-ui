a482020311bbdc088b4b1c9500dc32574444bcc6
Patrick Lee <patricianist@gmail.com>
2024-01-07 08:45:32 +0800

    update: added domain

--- utils/app/api.ts
+++ utils/app/api.ts
@@ -1,13 +1,15 @@
 import { Plugin, PluginID } from '@/types/plugin';
 
+const domain = 'https://localhost:8000'
+
 export const getEndpoint = (plugin: Plugin | null) => {
   if (!plugin) {
-    return 'chat';
+    return `${domain}/chat`;
   }
 
   if (plugin.id === PluginID.GOOGLE_SEARCH) {
     return 'api/google';
   }
 
-  return 'chat';
+  return `${domain}/chat`;
 };
